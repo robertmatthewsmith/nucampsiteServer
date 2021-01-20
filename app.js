5dbe0b30f9f88c0e4e41f516b72b22ec083cd604
Robert Smith <robertmatthewsmith1976@gmail.com>
2021-01-19 18:04:12 -0600

    CORS

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.all('*', (req, res, next) => {
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+  app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
