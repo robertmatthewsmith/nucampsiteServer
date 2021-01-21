f77a9b566c3080d501517097900d8ff7beabc978
Robert Smith <robertmatthewsmith1976@gmail.com>
2021-01-20 18:49:24 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '978586639337352',
+        clientSecret: '38fc64b152c90423e0500e9b1467a340'
+    }
 }
